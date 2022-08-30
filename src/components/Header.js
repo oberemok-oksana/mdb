56f5908d77f8175998564ff95b1d54637b54a954
Oksana Oberemok <Pylypenkok@gmail.com>
2022-08-30 21:43:34 +0800

    made Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,57 @@
 import styles from "./Header.module.css";
-import Link from "react-router-dom";
+import Container from "./ui/Container";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <div className={styles["left-navbar"]}>
-        <div>Logo</div>
-        <input type="search" />
-      </div>
-      <nav>
-        <ul className={styles.nav}>
-          <li>Top 10 movies</li>
-          <li>My watching list</li>
-          <li>Login</li>
-          <li>Logout</li>
-        </ul>
-      </nav>
+      <Container className={styles.flex}>
+        <div className={styles["left-navbar"]}>
+          <div className={styles.logo}>KMDb</div>
+          <input type="search" placeholder="Search" />
+        </div>
+        <nav>
+          <ul className={styles.nav}>
+            <li>
+              <NavLink
+                to="/"
+                activeClassName={styles.active}
+                className={styles.item}
+                exact
+              >
+                Top 10 movies |
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/my-watching-list"
+                activeClassName={styles.active}
+                className={styles.item}
+              >
+                My watching list |
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/login"
+                activeClassName={styles.active}
+                className={styles.item}
+              >
+                Login |
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/logout"
+                activeClassName={styles.active}
+                className={styles.item}
+              >
+                Logout
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </Container>
     </header>
   );
 };
