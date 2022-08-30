daac4806ff321d53f71d76e17e77013be2e76741
Oksana Oberemok <Pylypenkok@gmail.com>
2022-08-30 21:44:37 +0800

    add routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
 import "./App.css";
+import { Switch, Route, Redirect } from "react-router-dom";
+import HeaderPage from "../src/pages/HeaderPage";
+import HomePage from "./pages/HomePage";
+import MyWatchingListPage from "./pages/MyWatchingListPage";
+import LoginPage from "./pages/LoginPage";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <HeaderPage />
+        <Switch>
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
+          <Route path="/my-watching-list">
+            <MyWatchingListPage />
+          </Route>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+          <Route path="*">
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      </div>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
