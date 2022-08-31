af6a5c7352f6653522b4fa601d45731c57b4f71f
Oksana Oberemok <Pylypenkok@gmail.com>
2022-08-31 17:38:11 +0800

    changed movie component with inner data

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,9 +1,25 @@
 import styles from "./Movie.module.css";
+import { Link, useHistory } from "react-router-dom";
 
 const Movie = (props) => {
+  const history = useHistory();
+
+  const showDetails = (id) => {
+    history.push(`/movies/${id}`);
+  };
+
   return (
-    <li key={props.id} className={styles.item}>
-      {props.title}
+    <li
+      key={props.id}
+      className={styles.item}
+      onClick={() => showDetails(props.id)}
+    >
+      {/* <Link to={`/movies/${props.id}`}> */}
+      <h3>{props.title}</h3>
+      <span>
+        (average vote: <span className={styles.vote}> {props.vote})</span>
+      </span>
+      {/* </Link> */}
     </li>
   );
 };
