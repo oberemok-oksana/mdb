58f0adf5ed8dc374ff1386ee0fddc997da8203f0
Oksana Oberemok <Pylypenkok@gmail.com>
2022-09-05 19:44:16 +0800

    created SearchForm component

---
+++ src/components/SearchForm.js
@@ -0,0 +1,38 @@
+import Button from "@mui/material/Button";
+import styles from "./SearchForm.module.css";
+import { useState } from "react";
+import { findMovie } from "../api";
+import { useHistory } from "react-router-dom";
+
+const SearchForm = () => {
+  const [searchValue, setSearchValue] = useState("");
+  const history = useHistory();
+
+  const changeSearchValue = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    console.log(searchValue);
+
+    // findMovie(searchValue);
+    history.push(`/searching-results/${searchValue}`);
+  };
+
+  return (
+    <form className={styles.form} onSubmit={submitForm}>
+      <input
+        type="search"
+        placeholder="Search"
+        value={searchValue}
+        onChange={changeSearchValue}
+      />
+      <Button size="small" variant="contained" type="submit">
+        Search
+      </Button>
+    </form>
+  );
+};
+
+export default SearchForm;
